b7ca7b2432687d1c86a3ad5c7ac81c806ee8adaa
yeasintamim <yeasin47@student.sust.edu>
2021-10-10 18:27:11 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Sidebar } from "./component/Sidebar";
+import { Navbar } from "./component/Navbar";
+import { Form } from "./component/Route";
+import { Cards } from "./component/Cards";
+
+import "./App.css";
+
+
+export const App = () => {
+    return (
+      <div className="row">
+           <Sidebar />
+        <div className="col-10 p-0 border-start">
+            <Navbar />
+            <div className="p-3">
+                <h3 className="m-4">Welcome, Find a felxible flight for your next trip</h3>    
+            </div>
+            <Form />
+            <Cards />
+        </div>
+     </div>
+    );
+  };
+
+export default App;
